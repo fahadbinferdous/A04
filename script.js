fdef53f6490460bcc91574cad0058758be0275e8
fahadbinferdous <bin.ferdous@gmail.com>
2026-02-22 21:07:45 +0600

    third commit dashboard js created

---
+++ script.js
@@ -0,0 +1,26 @@
+// dashboard //
+
+let totalCount=document.getElementById('total-count');   // get data from dashboard
+let interviewCount=document.getElementById('interview-count')
+let rejectedCount=document.getElementById('rejected-count')
+
+
+let availableJobs=document.getElementById('available-jobs') // get data from available jobs
+let interviewJobs=document.getElementById('interview-jobs')
+let rejectedJobs=document.getElementById('rejected-jobs')
+
+
+                                                            
+function calculateCount(){
+    totalCount.innerText=availableJobs.children.length     // set updated data into dashboard from available jobs
+    interviewCount.innerText=interviewJobs.children.length
+    rejectedCount.innerText=rejectedJobs.children.length
+} 
+
+calculateCount()    //call the function to be effective
+
+
+
+
+
+
